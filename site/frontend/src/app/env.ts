72e1d7f14da093766803d9099d372ef80e5b15f3
Callum Coy <gamecc@ymail.com>
2020-11-15 09:13:39 -0500

    ip for when theathered

--- site/frontend/src/app/env.ts
+++ site/frontend/src/app/env.ts
@@ -1,1 +1,2 @@
-export const API_URL = '192.168.1.153:5000';
+//export const API_URL = '192.168.1.153:5000'; // for testing on personal wifi
+export const API_URL = '10.42.0.1:5000'; // for local access
