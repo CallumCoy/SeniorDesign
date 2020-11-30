196f72faad664ffd88acb847aee93e301485228d
Callum Coy <callummichaelcoy242@gmail.com>
2020-11-30 18:40:27 -0500

    changed it to the final IP

--- site/frontend/src/app/env.ts
+++ site/frontend/src/app/env.ts
@@ -1,2 +1,2 @@
-export const API_URL = '192.168.1.157:5000'; // for testing on personal wifi
-//export const API_URL = '10.42.0.1:5000'; // for local access
+//export const API_URL = '192.168.1.157:5000'; // for testing on personal wifi
+export const API_URL = '10.42.0.1:5000'; // for local access
